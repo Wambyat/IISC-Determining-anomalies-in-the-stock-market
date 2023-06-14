8793a7f0cc0783fd1618d1453b1f443e227acb55
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-06-14 19:44:53 +0530

    adding a few comments

--- Streamlit Tests/profile.py
+++ Streamlit Tests/profile.py
@@ -1,3 +1,4 @@
 import streamlit as st
 def profile_page():
     st.write("This is profile page")
+    #OPtional tbh prob shou;d add something here
--- Streamlit Tests/stocks.py
+++ Streamlit Tests/stocks.py
@@ -1,3 +1,4 @@
 import streamlit as st
 def stocks_page():
     st.write("This is stocks page")
+    #GOnna put the graphs n shit here.
