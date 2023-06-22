e29da38d73a9fecf63a09194844d9e6c55769988
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-06-22 14:30:32 +0530

    Update merger.py

--- General Tests/merger.py
+++ General Tests/merger.py
@@ -9,8 +9,18 @@ print(joined_files)
 # A list of all joined files is returned
 joined_list = glob.glob(joined_files)
 
-print(joined_list)
-  
-# Finally, the files are joined
-df = pd.concat(map(pd.read_csv, joined_list), ignore_index=True)
-print(df)
+# print(joined_list)
+print("Join of dir done.")
+
+def merge_text_files(file_list, output_file):
+    with open(output_file, 'w') as outfile:
+        for file_name in file_list:
+            with open(file_name, 'r') as infile:
+                outfile.write(infile.read())
+                outfile.write('\n')  # Add a new line after each file's content
+
+# Example usage
+file_list = joined_list
+output_file = 'merged.csv'
+merge_text_files(file_list, output_file)
+print("Files merged!!!!!!!!!!!!!!!!!!!!!!!!!")
