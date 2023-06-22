e8c8f3593f490d83c012aad20668b236d6edfd29
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-06-22 14:44:14 +0530

    final merger.py

--- General Tests/merger.py
+++ General Tests/merger.py
@@ -2,16 +2,11 @@
 import pandas as pd
 import glob
 import os
-  
-# merging the files
-joined_files = os.path.join("./data", "*.csv")
-print(joined_files)
-# A list of all joined files is returned
-joined_list = glob.glob(joined_files)
 
-# print(joined_list)
-print("Join of dir done.")
-
+#This function takes a list of file names and merges them into one output file.
+# @param file_list: A list of file names to be merged
+# @param output_file: The output file name
+# @return: None
 def merge_text_files(file_list, output_file):
     with open(output_file, 'w') as outfile:
         for file_name in file_list:
@@ -19,8 +14,20 @@ def merge_text_files(file_list, output_file):
                 outfile.write(infile.read())
                 outfile.write('\n')  # Add a new line after each file's content
 
-# Example usage
-file_list = joined_list
+
+# creating the path string
+joined_files = os.path.join("./data", "*.csv")
+
+# a list of all csv files in the directory
+joined_list = glob.glob(joined_files)
+# print(joined_list)
+print("Join of dir done.")
+
 output_file = 'merged.csv'
-merge_text_files(file_list, output_file)
-print("Files merged!!!!!!!!!!!!!!!!!!!!!!!!!")
+merge_text_files(joined_list, output_file)
+
+# print("Files merged!!!!!!!!!!!!!!!!!!!!!!!!!")
+# this is creacking if pandas can handle the file
+df = pd.read_csv(output_file)
+print(df.head())
+print(df.shape)
