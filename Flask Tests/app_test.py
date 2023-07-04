b9d2519411a5a51fdd6afc7e5c308c75b166edda
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-07-04 12:01:46 +0530

    Form functionality added

--- Flask Tests/app_test.py
+++ Flask Tests/app_test.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/formtest', methods=['GET', 'POST'])
+def formtest():
+    #checking if it is a post or get method
+    if request.method == 'POST':
+        return render_template('formtest.html', req_type = request.method, form_data = request.form)
+    else:
+        return render_template('formtest.html', req_type = request.method)
 
 if __name__ == '__main__':
     app.run(debug=True)
