5bf9f81e9b3af4e3a7fcd95d79df60e846669f1a
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-07-10 11:46:15 +0530

    got some data

--- Tools/NSE_data_retrival.py
+++ Tools/NSE_data_retrival.py
@@ -1,5 +1,9 @@
 from nselib import capital_market
 import pandas
+import nselib
 
-data = capital_market.price_volume_and_deliverable_position_data(symbol='SBIN', from_date='01-06-2023', to_date='10-06-2023')
+comp = input("Company ticker: ")
+
+
+data = capital_market.price_volume_and_deliverable_position_data(symbol=comp, from_date='06-06-2023', to_date='06-07-2023')
 print(data)
