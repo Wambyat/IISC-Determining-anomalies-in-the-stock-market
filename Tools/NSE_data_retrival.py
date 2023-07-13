2fc949f178afec4236d5cad3068ba34692e5ad55
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-07-13 15:26:49 +0530

    Reddit is almost properly integrated.

--- Tools/NSE_data_retrival.py
+++ Tools/NSE_data_retrival.py
@@ -2,8 +2,15 @@ from nselib import capital_market
 import pandas
 import nselib
 
-comp = input("Company ticker: ")
+# comp = input("Company ticker: ")
 
 
-data = capital_market.price_volume_and_deliverable_position_data(symbol=comp, from_date='06-06-2023', to_date='06-07-2023')
-print(data)
+# data = capital_market.price_volume_and_deliverable_position_data(symbol=comp, from_date='06-06-2023', to_date='06-07-2023')
+comp = 'ZURAISJNK'
+data = capital_market.equity_list()
+b = data.loc[data['SYMBOL']==comp]['NAME OF COMPANY']
+
+try:
+    print(b.values[0])
+except IndexError:
+    print("Invalid ticker <only nse is supported>")
