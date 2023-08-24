52656c7a06303f183faedbb306af2256b4d209f2
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-08-24 20:07:38 +0530

    small fixes

--- Vue/proj/iiscproj/src/views/StockView.vue
+++ Vue/proj/iiscproj/src/views/StockView.vue
@@ -1,14 +1,8 @@
 <template>
     <div>
-        <h2>Search UWU</h2>
-        <p>{{ query }}</p>
-        <div v-for="(item, key) in news" :key="key">
-            <p>TICKER: {{ item.symbol }}</p>
-            <p>Desc: {{ item.desc }}</p>
-            <p>Company: {{ item.sm_name }}</p>
-            <p>Announcement Time: {{ item.an_dt }}</p>
-            <p>Full announcement: {{ item.attchmntText }}</p>
-        </div>
+        <h2>{{ query }}</h2>
+        <h2 id="compName">before</h2>
+        <p>grrr</p>
     </div>
 </template>
 
@@ -24,18 +18,28 @@
         },
         props: {
             query: String,
+            compName: String,
         },
         setup(props) {
             // news and dataToday are like global variables. They will be used in the template. query is from the prev page.
             const news = ref({});
             const dataToday = ref({});
+            let compName = ref(props.compName);
             const query = ref(props.query);
+            let comp = fetch("http://localhost:5000/api/all").then(
+                (response) => {
+                    const data = response.json().then((data) => {
+                        console.log(data);
+                        document.getElementById("compName").innerHTML =
+                            data[query.value];
+                    });
+                }
+            );
             onMounted(async () => {
-                console.log("getting data");
-
                 // This gets the latest data about the stock from nse. This is calling flask.
                 function getLatest() {
-                    const apiURL = "http://localhost:5000/api/stock";
+                    console.log(query.value);
+                    const apiURL = "http://localhost:5000/api/stock/";
                     return axios
                         .post(apiURL, {
                             ticker: query.value,
@@ -51,31 +55,8 @@
                 }
                 await getLatest();
                 console.log(dataToday.value);
-
-                // This gets the news about the stock from nse. This is calling express.
-                function getNews(a) {
-                    if (a > 5) {
-                        return;
-                    }
-                    const apiUrl =
-                        "http://localhost:3000/api/corporate-announcements/"+query.value;
-                    return axios
-                        .get(apiUrl)
-                        .then((response) => {
-                            console.log(response.data);
-                            news.value = response.data;
-                            return response.data;
-                        })
-                        .catch((error) => {
-                            console.error(error);
-                            console.log("retrying");
-                            getNews(a + 1);
-                        });
-                }
-                await getNews(0);
-                console.log(news.value);
             });
-            return { news, dataToday };
+            return { dataToday };
         },
     };
 </script>
