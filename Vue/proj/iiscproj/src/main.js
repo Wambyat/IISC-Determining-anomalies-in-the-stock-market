7d84417970dc5e876c074d3581748549f44f3c08
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-08-25 10:54:12 +0530

    THE GRAPH WORKS!!!

--- Vue/proj/iiscproj/src/main.js
+++ Vue/proj/iiscproj/src/main.js
@@ -3,5 +3,16 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import Highcharts from "highcharts";
+import StockModule from "highcharts/modules/stock";
+import HighchartsVue from "highcharts-vue";
 
-createApp(App).use(router).mount('#app')
+StockModule(Highcharts);
+
+const app = createApp(App);
+
+// Use the HighchartsVue plugin, register <highcharts> component
+app.use(HighchartsVue);
+app.use(router);
+// Mount the application
+app.mount("#app");
