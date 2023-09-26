69f638188c9bee4c493d62bfcd6bc927c1424928
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-09-26 13:49:39 +0530

    Update RUNME.bat

--- RUNME.bat
+++ RUNME.bat
@@ -1,6 +1,6 @@
 @echo off
 start cmd /k "echo This news server && cd "\Main Code\iiscproj" && node sideserver.js"
-start cmd /k "echo This is Flask server && cd "\Main Code\Scripts" && activate.bat"
+start cmd /k "echo This is Flask server && cd "\Main Code\Flask" && python3 app.py"
 echo "This is Vue server"
 cd "\Main Code\iiscproj"
 npm run serve
